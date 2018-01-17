b3476d2b06b8ceac7cd103682592f9b4ac35fe5d
gnosowsky <32077895+gnosowsky@users.noreply.github.com>
2018-01-17 14:13:10 +0100

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Character
+    {
+        public int X;
+        public int Y;
+        public int sizeX = 90;
+        public int sizeY = 35;
+
+        public void Snake()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.WriteLine("#");
+        }
+
+        public void Movement()
+        {
+            ConsoleKeyInfo keyInfo;
+
+            while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
+            {
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (Y <= 0)
+                            Y = 0;
+                        else
+                            Y--;
+
+                        UpdatePosition();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (X >= sizeX)
+                            X = sizeX;
+                        else
+                            X++;
+
+                        UpdatePosition();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (Y >= sizeY)
+                            Y = sizeY;
+                        else
+                            Y++;
+
+                        UpdatePosition();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (X <= 0)
+                            X = 0;
+                        else
+                            X--;
+
+                        UpdatePosition();
+                        break;
+                }
+            }
+            
+        }
+
+        public void UpdatePosition()
+        {
+            Console.Clear();
+            if (CheckPosition() == true)
+                Console.SetCursorPosition(X, Y);
+
+            Console.Write("#");
+            
+        }
+
+        public bool CheckPosition()
+        {
+            if (X < 0)
+            {
+                Console.SetCursorPosition(0, Y);
+                return false;
+            }
+
+            else if (X >= Console.WindowWidth)
+            {
+                Console.SetCursorPosition(sizeX, Y);
+                return false;
+            }
+
+            else if (Y < 0)
+            {
+                Console.SetCursorPosition(X, 0);
+                return false;
+            }
+
+            else if (Y >= Console.WindowHeight)
+            {
+                Console.SetCursorPosition(X, sizeY);
+            }
+
+            return true;
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character character = new Character();
+            Food food = new Food();
+            Console.CursorVisible = false;
+
+            Console.SetWindowSize(character.sizeX, character.sizeY);
+
+            character.Snake();
+            food.Position();
+            character.Movement();
+
+            Console.ReadKey();
+        }
+    }
+}
